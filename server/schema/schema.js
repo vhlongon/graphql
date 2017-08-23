dc004d496e4a7bb895b7d688c7282a916ce9b335
Victor Hugo Longon <victor.longon@etraveli.com>
2017-08-23 19:40:02 +0200

    add companies end point and node-fetch to get data async on the schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,14 +1,22 @@
 const graphl = require('graphql');
+const fetch = require('node-fetch');
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphl;
 
-const users = [
-  { id: '1', firstName: 'Martin', age: 33 },
-  { id: '2', firstName: 'Louis', age: 20 }
-];
+const getUsers = async () => {
+  try {
+    const response = await fetch(`http://localhost:8084/users`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
 
-const getUser = (collection, args) =>
-  collection.reduce((acc, user) => (user.id === args.id ? user : acc), {});
+const getUser = (args, prop, collection = getUsers()) =>
+  collection.then(users =>
+    users.reduce((acc, user) => (user[prop] === args[prop] ? user : acc), {})
+  );
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -26,7 +34,7 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return getUser(users, args);
+        return getUser(args, 'id');
       }
     }
   }
