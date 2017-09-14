e06581d5baa15b1d4baa94834eae8ae989e4813c
Victor Hugo Longon <victor.longon@etraveli.com>
2017-09-14 23:42:24 +0200

    add mutation to add a song

--- client/components/song-create.jsx
+++ client/components/song-create.jsx
@@ -1,10 +1,58 @@
 import React from 'react';
-import { withState } from 'recompose';
+import {
+  withStateHandlers,
+  withState,
+  withHandlers,
+  withProps,
+  compose,
+} from 'recompose';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
-const SongCreate = () =>
+const mutation = gql`
+  mutation addSong($title: String) {
+    addSong(title: $title) {
+      id
+      title
+    }
+  }
+`;
+
+const SongCreate = ({ title, updateTitle, onSubmit, mutate }) =>
   <div>
     <h3>Create a new song</h3>
-    <form />
+    <form onSubmit={onSubmit}>
+      <div>
+        <label htmlFor="title">Song Title:</label>
+        <input onChange={updateTitle} value={title} name="title" />
+      </div>
+      <input type="submit" value="hit me!" />
+    </form>
   </div>;
 
-export default withState()(SongCreate);
+const enhance = compose(
+  graphql(mutation),
+  withStateHandlers(
+    ({ title = '' }) => ({
+      title,
+    }),
+    {
+      updateTitle: () => ({ target }) => ({
+        title: target.value,
+      }),
+    }
+  ),
+  withHandlers({
+    onSubmit: ({ title, mutate }) => e => {
+      e.preventDefault();
+      console.log('submitting form with title: ', title);
+      mutate({
+        variables: {
+          title,
+        },
+      });
+    },
+  })
+);
+
+export default enhance(SongCreate);
